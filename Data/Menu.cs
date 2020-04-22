cafa6fd0fd8808175eb076096d50c44d1d7e548a
Zachery Brunner <zacheryBrunner@github.com>
2020-04-22 13:22:18 -0500

    Milestone requirements almost complete. Still need persistent checkboxes

--- Data/Menu.cs
+++ Data/Menu.cs
@@ -13,13 +13,27 @@ namespace CowboyCafe.Data
     public static class Menu
     {
         /// <summary>
+        /// All types of items in the menu
+        /// </summary>
+        public static string[] TypeOfItems
+        {
+            get => new string[]
+            {
+                "Entrees",
+                "Sides",
+                "Drinks"
+            };
+        }
+
+        /// <summary>
         /// Gets all of the entrees in the menu
         /// </summary>
-        /// <returns></returns>
+        /// <returns>All entrees at the Cowboy-Cafe</returns>
         public static IEnumerable<IOrderItem> Entrees()
         {
             List<IOrderItem> entrees = new List<IOrderItem>();
 
+            /* Add all of the entrees */
             entrees.Add(new AngryChicken());
             entrees.Add(new CowpokeChili());
             entrees.Add(new DakotaDoubleBurger());
@@ -34,11 +48,12 @@ namespace CowboyCafe.Data
         /// <summary>
         /// Get all the sides in the menu
         /// </summary>
-        /// <returns></returns>
+        /// <returns>All Sides at the Cowboy-Cafe</returns>
         public static IEnumerable<IOrderItem> Sides()
         {
             List<IOrderItem> sides = new List<IOrderItem>();
 
+            /* Add all of the sides */
             sides.Add(new BakedBeans());
             sides.Add(new ChiliCheeseFries());
             sides.Add(new PanDeCampo());
@@ -50,11 +65,12 @@ namespace CowboyCafe.Data
         /// <summary>
         /// Get all the drinks in the menu
         /// </summary>
-        /// <returns></returns>
+        /// <returns>All drinks at the Cowboy-Cafe</returns>
         public static IEnumerable<IOrderItem> Drinks()
         {
             List<IOrderItem> drinks = new List<IOrderItem>();
 
+            /* Add all of the drinks */
             drinks.Add(new CowboyCoffee());
             drinks.Add(new JerkedSoda());
             drinks.Add(new TexasTea());
@@ -66,27 +82,168 @@ namespace CowboyCafe.Data
         /// <summary>
         /// Compile the entire menu using the above 3 methods
         /// </summary>
-        /// <returns></returns>
+        /// <returns>The complete Cowboy-Cafe Menu</returns>
         public static IEnumerable<IOrderItem> CompleteMenu()
         {
             List<IOrderItem> completeMenu = new List<IOrderItem>();
 
-            foreach (IOrderItem i in Entrees())
-            {
-                completeMenu.Add(i);
-            }
-
-            foreach (IOrderItem i in Drinks())
-            {
-                completeMenu.Add(i);
-            }
-
-            foreach (IOrderItem i in Sides())
-            {
-                completeMenu.Add(i);
-            }
+            /* The AddRange Method concats 2 list objects together */
+            completeMenu.AddRange(Entrees());
+            completeMenu.AddRange(Sides());
+            completeMenu.AddRange(Drinks());
 
             return completeMenu;
+        }
+
+        /// <summary>
+        /// This is the functionality for the search bar in the website
+        /// </summary>
+        /// <param name="menu">The full menu</param>
+        /// <param name="terms">The search key to filter by</param>
+        /// <returns>An updated list with the filtered items</returns>
+        public static IEnumerable<IOrderItem> Search(IEnumerable<IOrderItem> menu, string terms)
+        {
+            /* See if terms are null, if so no need to go further */
+            if (terms == null) return menu;
+
+            /* Cast the menu to something you can work with */
+            List<IOrderItem> results = new List<IOrderItem>();
+            
+            /* Filter through the entire menu looking for the substring "terms" 
+                    If substring is found in the name then it is added to results list */
+            foreach(IOrderItem item in menu)
+                if(item.Name.Contains(terms, System.StringComparison.InvariantCultureIgnoreCase))
+                    results.Add(item);
+
+            return results;
+        }
+
+        /// <summary>
+        /// Filters by the categories of the IOrderItems
+        /// </summary>
+        /// <param name="menu">The current filtered menu</param>
+        /// <param name="cat">The list of categories to filter by</param>
+        /// <returns>An updated list with the filtered items</returns>
+        public static IEnumerable<IOrderItem> FilterByCategory(IEnumerable<IOrderItem> menu, IEnumerable<string> cat)
+        {
+            List<IOrderItem> results = new List<IOrderItem>();
+
+            /* Create some boolean values so I only have to traverse list once */
+            bool filterInEntrees = ((List<string>)cat).Contains("Entrees");
+            bool filterInSides = ((List<string>)cat).Contains("Sides");
+            bool filterInDrinks = ((List<string>)cat).Contains("Drinks");
+
+            foreach(IOrderItem i in menu)
+            {
+                /* Check type of item */
+                if (i is Entree e)
+                {
+                    /* Check if we include item */
+                    if (filterInEntrees)
+                        results.Add(e);
+                }
+
+                else if (i is Drink d)
+                {
+                    if (filterInDrinks)
+                        results.Add(d);
+                }
+
+                else if (i is Side)
+                {
+                    if(filterInSides)
+                        results.Add(i);
+                }    
+            }
+
+            /* Return the update search */
+            return results;
+        }
+
+        /// <summary>
+        /// Filters by the calories of the items
+        /// </summary>
+        /// <param name="menu">The current menu</param>
+        /// <param name="min">The minimum number of calories that must be in the item</param>
+        /// <param name="max">The maximum number of calories that must be in the item</param>
+        /// <returns>An updated list with the filtered items</returns>
+        public static IEnumerable<IOrderItem> FilterByCalories(IEnumerable<IOrderItem> menu, int? min, int? max)
+        {
+            /* Return the entire menu, we don't need to filter */
+            if (min == null && max == null) return menu;
+
+            /* Create results list that we can populate */
+            List<IOrderItem> results = new List<IOrderItem>();
+
+            /* Filter by the max */
+            if(min == null)
+            {
+                foreach (IOrderItem i in menu)
+                    if (i.Calories <= max)
+                        results.Add(i);
+            }
+
+            /* Filter by the min */
+            else if(max == null)
+            {
+                foreach (IOrderItem i in menu)
+                    if (i.Calories >= min)
+                        results.Add(i);
+            }
+
+            /* Min and max are both not null */
+            else
+            {
+                foreach (IOrderItem i in menu)
+                    if (i.Calories >= min && i.Calories <= max)
+                        results.Add(i);
+            }
+
+            /* Return the updated search */
+            return results;
+        }
+
+        /// <summary>
+        /// Filters by the price of the entree
+        /// </summary>
+        /// <param name="menu">The current filtered menu</param>
+        /// <param name="min">The minimum price of the entree to filter by</param>
+        /// <param name="max">The maximum price of the entree to filter by</param>
+        /// <returns>An updated list with the filtered items</returns>
+        public static IEnumerable<IOrderItem> FilterByPrice(IEnumerable<IOrderItem> menu, double? min, double? max)
+        {
+            /* Nothing to filter, return here */
+            if (min == null && max == null) return menu;
+
+            /* Create results list that we can populate */
+            List<IOrderItem> results = new List<IOrderItem>();
+            
+            /* Filter by the max */
+            if (min == null)
+            {
+                foreach (IOrderItem i in menu)
+                    if (i.Price <= max)
+                        results.Add(i);
+            }
+
+            /* Filter by the min */
+            else if (max == null)
+            {
+                foreach (IOrderItem i in menu)
+                    if (i.Price >= min)
+                        results.Add(i);
+            }
+
+            /* Min and max are both not null */
+            else
+            {
+                foreach (IOrderItem i in menu)
+                    if (i.Price >= min && i.Price <= max)
+                        results.Add(i);
+            }
+
+            /* Return the updated search */
+            return results;
         }
     }
 }
--- Website/Pages/Index.cshtml.cs
+++ Website/Pages/Index.cshtml.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
 using CowboyCafe.Data;
+using CCMenu = CowboyCafe.Data.Menu;
 using System.Text;
 
 namespace Website.Pages
@@ -15,14 +16,77 @@ namespace Website.Pages
     {
         private readonly ILogger<IndexModel> _logger;
 
+        /// <summary>
+        /// Holds the current menu that is shown in the html
+        /// </summary>
+        public IEnumerable<IOrderItem> Menu { get; protected set; }
+
+
+        [BindProperty]
+        public string[] typeOfItems { get; set; }
+
+        /// <summary>
+        /// Search terms if the user wanted to search for something
+        /// </summary>
+        [BindProperty]
+        public string SearchTerms { get; set; } = "";
+
+        /// <summary>
+        /// The minimum price to filter the items by
+        /// </summary>
+        [BindProperty]
+        public double? PriceMin { get; set; } = 0;
+
+        /// <summary>
+        /// The maximum price to filter the items by
+        /// </summary>
+        [BindProperty]
+        public double? PriceMax { get; set; } = 100;
+
+        /// <summary>
+        /// The minimum calorie to filter the items by
+        /// </summary>
+        [BindProperty]
+        public int? CalorieMin { get; set; } = 0;
+
+        /// <summary>
+        /// The maximum calorie to filter the items by
+        /// </summary>
+        [BindProperty]
+        public int? CalorieMax { get; set; } = 1000;
+
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
         }
 
-        public void OnGet()
+        /// <summary>
+        /// Hits this method when get request start flowing
+        /// </summary>
+        public void OnGet(int? CalorieMin, int? CalorieMax, double? PriceMin, double? PriceMax, string[]? typeOfItems)
         {
-            
+            this.CalorieMax = CalorieMax;
+            this.CalorieMin = CalorieMin;
+            this.PriceMin = PriceMin;
+            this.PriceMax = PriceMax;
+
+            SearchTerms = Request.Query["SearchTerms"];
+            typeOfItems = Request.Query["typeOfItems"];
+
+            if(typeOfItems.Length == 0)
+            {
+                typeOfItems = new string[]
+                {
+                    "Entrees",
+                    "Sides",
+                    "Drinks"
+                };
+            }
+
+            Menu = CCMenu.Search(CCMenu.CompleteMenu(), SearchTerms);
+            Menu = CCMenu.FilterByCalories(Menu, CalorieMin, CalorieMax);
+            Menu = CCMenu.FilterByCategory(Menu, typeOfItems.ToList());
+            Menu = CCMenu.FilterByPrice(Menu, PriceMin, PriceMax);
         }
     }
 }
